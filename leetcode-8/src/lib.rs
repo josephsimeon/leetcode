af92c1f041fa87afe523bfdb14a0f7a7af6ab526
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-12 18:06:36 +1000

    Added another test and altered the function based on it

--- leetcode-8/src/lib.rs
+++ leetcode-8/src/lib.rs
@@ -1,5 +1,5 @@
 fn my_atoi(s: String) -> i32 {
-    s.parse::<i32>().unwrap()
+    s.trim_start().parse::<i32>().unwrap()
 }
 
 #[cfg(test)]
@@ -9,5 +9,10 @@ mod test {
     #[test]
     fn test_42() {
         assert_eq!(my_atoi("42".to_string()), 42);
+    }
+
+    #[test]
+    fn test_spaced_neg42() {
+        assert_eq!(my_atoi(" -042".to_string()), -42);
     }
 }
