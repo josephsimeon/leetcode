e9f1c80b1dc8456052b589d00c9a024991bd1a1b
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-12 17:13:50 +1000

    Added in the function 'reverse' and unit tests for 7

--- leetcode-7/src/lib.rs
+++ leetcode-7/src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn reverse(x: i32) -> i32 {
+    let sign = x.abs() / x;
+
+    let s_rev = x.abs().to_string().chars().rev().collect::<String>();
+
+    s_rev.parse::<i32>().unwrap() * sign
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_123() {
+        assert_eq!(reverse(123), 321);
+    }
+
+    #[test]
+    fn test_neg123() {
+        assert_eq!(reverse(-123), -321);
+    }
+
+    #[test]
+    fn test_120() {
+        assert_eq!(reverse(120), 21);
     }
 }
