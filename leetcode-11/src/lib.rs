780cc76a2371711fd010f8d8cdbf2e179d82ad43
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-14 02:27:50 +1000

    Creating a searching algo to find the largest size bucket within the graphed area based on the unit tests and information in 11

--- leetcode-11/src/lib.rs
+++ leetcode-11/src/lib.rs
@@ -1,5 +1,18 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::cmp::min;
+
+fn max_area(height: Vec<i32>) -> i32 {
+    let mut area = 0;
+
+    for (i, x) in height.iter().enumerate() {
+        for (j, y) in height.iter().enumerate() {
+            if i != j {
+                let size = min(x, y) * (i as i32 - j as i32).abs();
+                if size > area { area = size };
+            }
+        }
+    }
+
+    area
 }
 
 #[cfg(test)]
@@ -7,8 +20,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        let test: Vec<i32> = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
+        assert_eq!(max_area(test), 49);
+    }
+
+    #[test]
+    fn test_2() {
+        let test: Vec<i32> = vec![1, 1];
+        assert_eq!(max_area(test), 1);
     }
 }
