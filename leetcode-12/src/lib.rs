7ec1f9ef775091257a711dc42999699ee8e750a4
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-14 11:04:43 +1000

    Added more conversion of digits to roman numerals along with unit tests for 12

--- leetcode-12/src/lib.rs
+++ leetcode-12/src/lib.rs
@@ -2,24 +2,40 @@ fn int_to_roman(num: i32) -> String {
     let mut s: String = String::new();
 
     let mut num: i32 = num;
-    while num > 1 {
+    while num > 0 {
         let reduce;
         match num {
             num if num >= 1000 => {
                 s.push_str("M");
                 reduce = 1000;
             },
+            num if num >= 900 => {
+                s.push_str("CM");
+                reduce = 900;
+            },
             num if num >= 500 => {
                 s.push_str("D");
                 reduce = 500;
+            },
+            num if num >= 400 => {
+                s.push_str("CD");
+                reduce = 400;
             },
             num if num >= 100 => {
                 s.push_str("C");
                 reduce = 100;
             },
+            num if num >= 90 => {
+                s.push_str("XC");
+                reduce = 90;
+            },
             num if num >= 50 => {
                 s.push_str("L");
                 reduce = 50;
+            },
+            num if num >= 40 => {
+                s.push_str("XL");
+                reduce = 40;
             },
             num if num >= 10 => {
                 s.push_str("X");
@@ -55,7 +71,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_3769() {
-        assert_eq!(int_to_roman(3769), "MMMDCCLXIX");
+    fn test_3749() {
+        assert_eq!(int_to_roman(3749), "MMMDCCXLIX");
+    }
+
+    #[test]
+    fn test_58() {
+        assert_eq!(int_to_roman(58), "LVIII");
+    }
+
+    #[test]
+    fn test_1994() {
+        assert_eq!(int_to_roman(1994), "MCMXCIV");
     }
 }
