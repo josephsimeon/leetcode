f0f9c95c3b7b5770fbb6db794925139debfee9ba
Joseph Simeon <josephdmsimeon@icloud.com>
2025-11-14 10:55:17 +1000

    Added in a loop that will reduce the numeral via processing the string with a roman numeral for 12

--- leetcode-12/src/lib.rs
+++ leetcode-12/src/lib.rs
@@ -1,5 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn int_to_roman(num: i32) -> String {
+    let mut s: String = String::new();
+
+    let mut num: i32 = num;
+    while num > 1 {
+        let reduce;
+        match num {
+            num if num >= 1000 => {
+                s.push_str("M");
+                reduce = 1000;
+            },
+            num if num >= 500 => {
+                s.push_str("D");
+                reduce = 500;
+            },
+            num if num >= 100 => {
+                s.push_str("C");
+                reduce = 100;
+            },
+            num if num >= 50 => {
+                s.push_str("L");
+                reduce = 50;
+            },
+            num if num >= 10 => {
+                s.push_str("X");
+                reduce = 10;
+            },
+            num if num >= 9 => {
+                s.push_str("IX");
+                reduce = 9;
+            },
+            num if num >= 5 => {
+                s.push_str("V");
+                reduce = 5;
+            },
+            num if num >= 4 => {
+                s.push_str("IV");
+                reduce = 4;
+            },
+            num if num >= 1 => {
+                s.push_str("I");
+                reduce = 1;
+            },
+            _ => reduce = 0,
+        }
+
+        num -= reduce;
+    }
+
+    s
 }
 
 #[cfg(test)]
@@ -7,8 +55,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_3769() {
+        assert_eq!(int_to_roman(3769), "MMMDCCLXIX");
     }
 }
